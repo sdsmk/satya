f471c92f874b7a916a99ad4e6c8ff67f7c87a48a
sdsmk <34293493+sdsmk@users.noreply.github.com>
2018-07-04 17:16:00 +0530

    Create c.py

---
+++ c.py
@@ -0,0 +1,8 @@
+char = raw_input()
+if char.isalpha():
+  if char =='a'or char=='e'or char=='i'or char=='o'or char=='u':
+      print("is an vowel",char)
+  else:
+      print("is an consonant",char)
+else:
+  print"invalid input"
