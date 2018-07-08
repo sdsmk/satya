308c31aad4681264794f5b37d12fc755e5df44ea
sdsmk <34293493+sdsmk@users.noreply.github.com>
2018-07-08 15:13:24 +0530

    Add files via upload

---
+++ prime1.py
@@ -0,0 +1,10 @@
+a=20
+b=40
+for num in range(a,b + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
