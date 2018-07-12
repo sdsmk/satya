aa743ed7405e034fb45e6ecb2c0939f42c4275d8
sdsmk <34293493+sdsmk@users.noreply.github.com>
2018-07-12 11:08:06 +0530

    Add files via upload

--- rev1.py
+++ rev1.py
@@ -1,3 +1,7 @@
-a=int(input("Enter a string: "))
-print("Reverse of the string is: ")
-print(a[::-1])
+n=int(input("Enter number: "))
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+print("Reverse of the number:",rev)
