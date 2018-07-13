8d2d3e916b278083e70630a14cd6f3cd6bf98428
sdsmk <34293493+sdsmk@users.noreply.github.com>
2018-07-13 12:05:12 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,14 @@
+def sumOfAP( a, d,n) :
+    sum = 0
+    i = 0
+    while i < n :
+        sum = sum + a
+        a = a + d
+        i = i + 1
+    return sum
+n = float(input())
+a = float(input())
+d = float(input())
+print (sumOfAP(a, d, n))
+ 
+
